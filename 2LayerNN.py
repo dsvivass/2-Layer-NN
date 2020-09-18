d3f4c85f066d36b174f13e927d2bdc81212d7363
dsvivass <dsvivass@unal.edu.co>
2020-09-18 13:38:29 -0500

    Se agrega opcion para plotear en executer.sh

--- 2LayerNN.py
+++ 2LayerNN.py
@@ -7,7 +7,7 @@ import math
 import csv
 import sys
 
-def rose(a=1, n=3, disp=2):
+def rose(a=1, n=3, disp=2, plot=True):
     '''Funcion para definir las rosas,
     disp = dispersion (mayor --> datos mas ajustados)'''
     # containing the radian values
@@ -18,7 +18,7 @@ def rose(a=1, n=3, disp=2):
 
     b = np.arange(0, rads.shape[0], rads.shape[0]/n)
 
-    for i,rad in enumerate(tqdm.tqdm(rads)):
+    for i,rad in enumerate(tqdm.tqdm(rads, desc = 'GENERANDO DATOS')):
         r = a * np.cos(n * rad) + np.random.randn(1) / disp
         x = np.squeeze(r*np.cos(rad))
         y = np.squeeze(r*np.sin(rad))
@@ -29,11 +29,12 @@ def rose(a=1, n=3, disp=2):
             plt.plot(r*np.cos(rad), r*np.sin(rad), np.random.choice(color2, 1)[0] + '.')
 
     # display the polar plot
-    plt.show()
+    if plot == True: plt.show()
 
 def main_nn():
-    n, a, disp = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])
-    rose(n=n, a=a, disp=disp)
+    n, a, disp, plot = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), sys.argv[4]
+    plot = plot == 'True' # True si se cumple la condicion, asi pasa de str a bool
+    rose(n=n, a=a, disp=disp, plot=plot)
 
 
 if __name__ == '__main__':
--- executer.sh
+++ executer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./2LayerNN.py 3 1 10
+# /script.py n_petalos r disp plot
+./2LayerNN.py 2 1 10 True
